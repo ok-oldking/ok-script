4d13c9554853f3f6dfd21205746cb7d4a03efc93
firedcto@gmail.com <firedcto@gmail.com>
2024-06-07 21:13:41 +0800

    try use downscaling for featureset  not working

--- ok/feature/Feature.py
+++ ok/feature/Feature.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 class Feature:
-    def __init__(self, mat: np.ndarray, x: int, y: int, width: int, height: int) -> None:
+    def __init__(self, mat: np.ndarray, x: int, y: int, width: int, height: int, frame_width: int,
+                 frame_height: int) -> None:
         """
         Initialize a Feature with an image (Mat) and its bounding box coordinates.
 
@@ -18,3 +19,8 @@ class Feature:
         self.y = y
         self.width = width
         self.height = height
+        self.frame_width = frame_width
+        self.frame_height = frame_height
+
+    def __str__(self) -> str:
+        return f'self.x: {self.x}, self.y: {self.y}, width: {self.width}, height: {self.height}, self.frame_width: {self.frame_width}, frame_height: {self.frame_height}'
