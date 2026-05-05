4f66d0433dde39867a345922739e0a0016d25508
AliceJump <2630923991@qq.com>
2026-05-05 23:55:15 +0800

    feat: update workflow for PyPI publishing and add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,34 +1,25 @@
 import os
 import setuptools
-import sys
-from get_pypi_latest_version import GetPyPiLatestVersion
 
-os.environ["PYTHONIOENCODING"] = "utf-8"
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 MODULE_NAME = "ok-script"
-FORCED_VERSION = os.environ.get("OK_SCRIPT_VERSION", "").strip()
 
-if FORCED_VERSION:
-    VERSION_NUM = FORCED_VERSION
-    print(f'forced version is {VERSION_NUM}')
-else:
-    obtainer = GetPyPiLatestVersion()
-    latest_version = obtainer(MODULE_NAME)
-    VERSION_NUM = obtainer.version_add_one(latest_version, add_patch=True)
-    print(f'latest_version is {latest_version} new version is {VERSION_NUM}')
+VERSION_NUM = os.environ.get("OK_SCRIPT_VERSION")
+if not VERSION_NUM:
+    raise RuntimeError("OK_SCRIPT_VERSION is required for build")
 
 setuptools.setup(
     name=MODULE_NAME,
     version=VERSION_NUM,
-    author="ok-oldking",
-    author_email="firedcto@gmail.com",
+    author="alicejump",
+    author_email="149395013+AliceJump@users.noreply.github.com",
     description="Automation with Computer Vision for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ok-oldking/ok-script",
-    packages=setuptools.find_packages(exclude=['tests', 'docs']),
+    url="https://github.com/alicejump/ok-script",
+    packages=setuptools.find_packages(exclude=["tests", "docs"]),
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -37,16 +28,16 @@ setuptools.setup(
         "Operating System :: Microsoft :: Windows",
     ],
     install_requires=[
-        'pywin32>=306',
-        'pyappify>=1.0.2',
-        'PySide6-Fluent-Widgets==1.8.3',
-        'typing-extensions>=4.11.0',
-        'requests>=2.32.3',
-        'psutil>=6.0.0',
-        'pydirectinput==1.0.4',
-        'pycaw==20240210',
-        'mouse==0.7.1'
+        "pywin32>=306",
+        "pyappify>=1.0.2",
+        "PySide6-Fluent-Widgets==1.8.3",
+        "typing-extensions>=4.11.0",
+        "requests>=2.32.3",
+        "psutil>=6.0.0",
+        "pydirectinput==1.0.4",
+        "pycaw==20240210",
+        "mouse==0.7.1",
     ],
-    python_requires='==3.12.*',
+    python_requires="==3.12.*",
     zip_safe=False,
 )
