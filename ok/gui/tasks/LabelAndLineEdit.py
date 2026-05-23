48ac49bf1566142f3d85fc923a5eb18fcf3c2c52
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2026-05-24 01:14:16 +0800

    Set sensible minimum width for line edits

--- ok/gui/tasks/LabelAndLineEdit.py
+++ ok/gui/tasks/LabelAndLineEdit.py
@@ -5,11 +5,14 @@ from ok.gui.tasks.ConfigLabelAndWidget import ConfigLabelAndWidget
 
 
 class LabelAndLineEdit(ConfigLabelAndWidget):
+    MIN_INPUT_WIDTH = 100
+    HORIZONTAL_PADDING = 32
 
     def __init__(self, config_desc, config, key: str):
         super().__init__(config_desc, config, key)
         self.key = key
         self.line_edit = LineEdit()
+        self.line_edit.setMinimumWidth(self.MIN_INPUT_WIDTH)
         self.update_value()
         self.line_edit.textChanged.connect(self.value_changed)
         self.add_widget(self.line_edit, stretch=0)
@@ -25,5 +28,5 @@ class LabelAndLineEdit(ConfigLabelAndWidget):
 
     def _update_width(self, value):
         font_metrics = QFontMetrics(self.line_edit.font())
-        content = value or "M" * 16
-        self.line_edit.setFixedWidth(font_metrics.horizontalAdvance(content) * 2)
+        content_width = font_metrics.horizontalAdvance(value or "") + self.HORIZONTAL_PADDING
+        self.line_edit.setFixedWidth(max(self.MIN_INPUT_WIDTH, content_width))
